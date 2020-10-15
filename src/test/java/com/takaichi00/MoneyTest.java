72012a5bb75a776efae0af7a768a8f154ef4346a
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-10-15 20:09:17 +0900

    chapter12: p85 create test

--- src/test/java/com/takaichi00/MoneyTest.java
+++ src/test/java/com/takaichi00/MoneyTest.java
@@ -2,6 +2,8 @@ package com.takaichi00;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
@@ -28,8 +30,11 @@ public class MoneyTest {
 
   @Test
   void testSimpleAddition() {
-    Money sum = Money.dollar(5).plus(Money.dollar(5));
-    assertEquals(Money.dollar(10), sum);
+    Money five = Money.dollar(5);
+    Expression sum = five.plus(five);
+    Bank bank = new Bank();
+    Money reduced = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(10), reduced);
   }
 
 }
