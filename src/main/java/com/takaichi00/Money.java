6863b13e1933a26bf0fae6e9ea300a26ae82ef2b
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-10-17 15:23:26 +0900

    chapter13: p92 add Sum.java

--- src/main/java/com/takaichi00/Money.java
+++ src/main/java/com/takaichi00/Money.java
@@ -36,6 +36,6 @@ class Money implements Expression {
   }
 
   public Expression plus(Money addend) {
-    return new Money(amount + addend.amount, currency);
+    return new Sum(this, addend);
   }
 }
