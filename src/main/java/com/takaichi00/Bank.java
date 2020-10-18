48bbe217b4d7452c56175b38fc83c6846aeeb77a
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-10-18 19:36:05 +0900

    chapter13: p94 add TODO List

--- src/main/java/com/takaichi00/Bank.java
+++ src/main/java/com/takaichi00/Bank.java
@@ -1,7 +1,8 @@
 package com.takaichi00;
 
 public class Bank {
-  public Money reduce(Expression sum, String usd) {
-    return Money.dollar(10);
+  public Money reduce(Expression source, String to) {
+    Sum sum = (Sum) source;
+    return sum.reduce(to);
   }
 }
--- src/test/java/com/takaichi00/MoneyTest.java
+++ src/test/java/com/takaichi00/MoneyTest.java
@@ -46,4 +46,12 @@ public class MoneyTest {
     assertEquals(five, sum.addend);
   }
 
+  @Test
+  void testReduceSum() {
+    Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+    Bank bank = new Bank();
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(7), result);
+  }
+
 }
