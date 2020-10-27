15a75332fb2f6c1cc095a20dcb17803dd7b84e69
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-10-27 21:15:05 +0900

    chapter16: add test but failed

--- src/test/java/com/takaichi00/MoneyTest.java
+++ src/test/java/com/takaichi00/MoneyTest.java
@@ -104,7 +104,11 @@ public class MoneyTest {
     Expression sum = new Sum(fiveBucks, tenFrances).times(2);
     Money result = bank.reduce(sum, "USD");
     assertEquals(Money.dollar(20), result);
-
   }
 
+  @Test
+  void testPlusSameCurrencyReturnMoney() {
+    Expression sum = Money.dollar(1).plus(Money.dollar(1));
+    assertTrue(sum instanceof Money);
+  }
 }
