fc6695330099e63cff3e4f71569beceb8e78e01b
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-10-27 21:13:00 +0900

    chapter16: p127 end

--- src/test/java/com/takaichi00/MoneyTest.java
+++ src/test/java/com/takaichi00/MoneyTest.java
@@ -95,4 +95,16 @@ public class MoneyTest {
     assertEquals(Money.dollar(15), result);
   }
 
+  @Test
+  void testSumTimes() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrances = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrances).times(2);
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(20), result);
+
+  }
+
 }
